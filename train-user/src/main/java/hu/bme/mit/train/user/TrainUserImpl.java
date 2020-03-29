d0182487950203e030c0a54b7878a73d8f20e9d1
csikosmarci24 <csikos.marci99@gmail.com>
2020-03-29 22:18:48 +0200

    Wait for 1000 instead of 100

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -34,7 +34,7 @@ public class TrainUserImpl implements TrainUser {
 			while (true) {
 				controller.followSpeed();
 				try {
-					Thread.sleep(100);
+					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
