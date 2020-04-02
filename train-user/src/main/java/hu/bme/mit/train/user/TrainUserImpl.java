a0bd87998e9b39c9a95195f7faddc6ba5323b526
Morvay Balázs <balazsmorvay@yahoo.com>
2020-04-02 16:12:14 +0200

    the wait is down to 100

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -34,7 +34,7 @@ public class TrainUserImpl implements TrainUser {
 			while (true) {
 				controller.followSpeed();
 				try {
-					Thread.sleep(500);
+					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
